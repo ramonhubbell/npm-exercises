2d6ab7d780b431f0c064f336811150ea289869f3
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-08-03 16:24:19 -0500

    fix changed files

---
+++ scan-capstone/src/main/java/com/scan4kids/project/daos/UsersRepository.java
@@ -0,0 +1,7 @@
+package com.scan4kids.project.daos;
+
+import com.scan4kids.project.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository <User, Long> {
+}
